4412a252e2aa1e7f44e32eeac03f8bcfcfaa8ed0
Sonveer Singh <sonveer.s@icreon.com>
2020-04-25 21:18:44 +0530

    Updates

---
+++ precache-manifest.e5288f35e450c9ebd8cc12f3ce90897e.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6085b89c154d9b8ff76a5e410459ed",
+    "url": "/sonveer/index.html"
+  },
+  {
+    "revision": "72d5a5a6f1bb7f4fb65c",
+    "url": "/sonveer/static/css/2.97b0828e.chunk.css"
+  },
+  {
+    "revision": "f95499f70059d76f4b38",
+    "url": "/sonveer/static/css/main.1527a83f.chunk.css"
+  },
+  {
+    "revision": "72d5a5a6f1bb7f4fb65c",
+    "url": "/sonveer/static/js/2.4cec5ad5.chunk.js"
+  },
+  {
+    "revision": "f95499f70059d76f4b38",
+    "url": "/sonveer/static/js/main.fe0fcd7b.chunk.js"
+  },
+  {
+    "revision": "a6370e2c43db26ad08c2",
+    "url": "/sonveer/static/js/runtime~main.1a4f15de.js"
+  },
+  {
+    "revision": "c9ae89a37de92cd02c5978e228e4e305",
+    "url": "/sonveer/static/media/about.c9ae89a3.jpg"
+  },
+  {
+    "revision": "10c5bb3ba78120f9b8e66f6faa425039",
+    "url": "/sonveer/static/media/auth.10c5bb3b.jpg"
+  },
+  {
+    "revision": "0ba959fd3605c7a8cc38eddb21278b8e",
+    "url": "/sonveer/static/media/avatar_1.0ba959fd.png"
+  },
+  {
+    "revision": "f7e7fc9ad4f2fcf6bb728967e9de6e39",
+    "url": "/sonveer/static/media/avatar_10.f7e7fc9a.png"
+  },
+  {
+    "revision": "031d7521994313e24e8c9f28db1fd59a",
+    "url": "/sonveer/static/media/avatar_11.031d7521.png"
+  },
+  {
+    "revision": "55f7378423ca75e44f1b2a5ffb340a33",
+    "url": "/sonveer/static/media/avatar_2.55f73784.png"
+  },
+  {
+    "revision": "504f7b4f1ab3a3b447d0e4ded9aaa835",
+    "url": "/sonveer/static/media/avatar_3.504f7b4f.png"
+  },
+  {
+    "revision": "03120898caa854650d1fdfa48c3b94ff",
+    "url": "/sonveer/static/media/avatar_4.03120898.png"
+  },
+  {
+    "revision": "864765d7b4afce2911a4dc8f4eb0ddd9",
+    "url": "/sonveer/static/media/avatar_5.864765d7.png"
+  },
+  {
+    "revision": "0db4eac2a38f37fd66f0310fb78a32fb",
+    "url": "/sonveer/static/media/avatar_6.0db4eac2.png"
+  },
+  {
+    "revision": "7b4b2e8666624cc3105267db6b211046",
+    "url": "/sonveer/static/media/avatar_7.7b4b2e86.png"
+  },
+  {
+    "revision": "bf9dc2bdbcccb072160cf28e6d886401",
+    "url": "/sonveer/static/media/avatar_8.bf9dc2bd.png"
+  },
+  {
+    "revision": "afd9e37a1e3d3248ebd97786b3ffc329",
+    "url": "/sonveer/static/media/avatar_9.afd9e37a.png"
+  },
+  {
+    "revision": "c4a99206a9cb84d984ff0c5b78c439a0",
+    "url": "/sonveer/static/media/banner.c4a99206.jpg"
+  },
+  {
+    "revision": "cfcba7294a8fceb273a2fee9115d3d0b",
+    "url": "/sonveer/static/media/banner1.cfcba729.jpg"
+  },
+  {
+    "revision": "ae1f3e9bace487e7ab1b3371b29ed53c",
+    "url": "/sonveer/static/media/blog_01.ae1f3e9b.jpg"
+  },
+  {
+    "revision": "f078ae75fc285fd767c04ccb0e93193f",
+    "url": "/sonveer/static/media/blog_05.f078ae75.jpg"
+  },
+  {
+    "revision": "d666e5fb97c81b2d99009af66e821613",
+    "url": "/sonveer/static/media/blog_masonry_03_04.d666e5fb.jpg"
+  },
+  {
+    "revision": "08fab11cfbf63c3517aba8cf247d44cb",
+    "url": "/sonveer/static/media/blog_square.08fab11c.jpg"
+  },
+  {
+    "revision": "9a9ec9a8b8ed1aca638d544f90f319a6",
+    "url": "/sonveer/static/media/cbse.9a9ec9a8.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sonveer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sonveer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sonveer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sonveer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sonveer/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d1a04c612130a4a352f15c93671c865e",
+    "url": "/sonveer/static/media/footer.d1a04c61.png"
+  },
+  {
+    "revision": "4a57404ba4e042fb72d1079fca43d65a",
+    "url": "/sonveer/static/media/logo--white.4a57404b.svg"
+  },
+  {
+    "revision": "8648ceea55615ccaa739455dcb289f95",
+    "url": "/sonveer/static/media/menu.8648ceea.jpg"
+  },
+  {
+    "revision": "620e8539ec0305d555c9cbf245ebca63",
+    "url": "/sonveer/static/media/myphoto.620e8539.png"
+  },
+  {
+    "revision": "2901ef992038c1a4ec7f571cab407c12",
+    "url": "/sonveer/static/media/portfolio1.2901ef99.jpg"
+  },
+  {
+    "revision": "7371adf27fe2e2f3e9c9e556f26a3e14",
+    "url": "/sonveer/static/media/portfolio2.7371adf2.jpg"
+  },
+  {
+    "revision": "d2fb2a87dc0af3aefe39e8cbebce4fad",
+    "url": "/sonveer/static/media/portfolio3.d2fb2a87.jpg"
+  },
+  {
+    "revision": "490dde8ae3c749d198b80691711f1281",
+    "url": "/sonveer/static/media/portfolio4.490dde8a.jpg"
+  },
+  {
+    "revision": "43c010796fa9a2c68f7d34512d1f228b",
+    "url": "/sonveer/static/media/portfolio5.43c01079.jpg"
+  },
+  {
+    "revision": "5938ed89ba3a086d87543306857b77df",
+    "url": "/sonveer/static/media/portfolio6.5938ed89.jpg"
+  },
+  {
+    "revision": "de45bbb383e3ec76069f6a7ba7b89913",
+    "url": "/sonveer/static/media/s1.de45bbb3.png"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/sonveer/static/media/undraw_page_not_found_su7k.6695af99.svg"
+  },
+  {
+    "revision": "fd77e55e562daef2619ebf7e299665fa",
+    "url": "/sonveer/static/media/undraw_resume_folder_2_arse.fd77e55e.svg"
+  },
+  {
+    "revision": "41b633417ffb9fbf27caea2be281cfe2",
+    "url": "/sonveer/static/media/uptu.41b63341.jpg"
+  }
+]);
